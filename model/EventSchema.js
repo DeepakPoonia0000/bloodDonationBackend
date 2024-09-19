a99106e50ee605d691c0ed98a4f5f5f91115ae73
DeepakPoonia0000 <dpoonia0000@gmail.com>
2024-09-19 13:08:33 +0530

    back

--- model/EventSchema.js
+++ model/EventSchema.js
@@ -16,11 +16,9 @@ const event = new mongoose.Schema({
         type: Date,
         default: function () {
             const eventDate = new Date(this.eventDate);
-            // Set delete time to 11:55 PM of the next day
-            const deleteAt = new Date(eventDate);
-            deleteAt.setDate(eventDate.getDate() + 1);  // Move to next day
-            deleteAt.setHours(23, 55, 0, 0);  // Set time to 11:55 PM
-            return deleteAt;
+            // Set delete time to 11:55 PM of the same day as eventDate
+            eventDate.setHours(23, 55, 0, 0);  // Set time to 11:55 PM
+            return eventDate;
         },
     },
 });
